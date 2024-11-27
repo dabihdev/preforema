ccaae9f5c1cdbd38291c947ee5d45bda5318137f
dabihdev <dabihdev@gmail.com>
2024-11-27 12:12:26 +0100

    creata classe UI

---
+++ app/ui.py
@@ -0,0 +1,29 @@
+from settings import *      # import global settings
+
+class UI:
+    def __init__(self):
+        pass
+
+    def display_commands(self):
+        pass
+
+    def prompt_user(self):
+        pass
+
+    def select_forecast_day(self):
+        pass
+
+    def create_project(self):
+        pass
+
+    def export_to_html(self):
+        pass
+
+    def show_info(self):
+        pass
+
+    def exit_program(self):
+        pass
+
+    def run(self):
+        pass
